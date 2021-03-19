3559dc0c735d0774fabc34572fef0c38b96a7bbc
Tom Randles <randles.tom@gmail.com>
2021-03-19 10:43:18 +0000

    More in-memory Db test updtes

--- SamuraiApp.Data/BusinessDataLogic.cs
+++ SamuraiApp.Data/BusinessDataLogic.cs
@@ -1,5 +1,7 @@
-﻿using SamuraiApp.Data.Database;
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data.Database;
 using SamuraiApp.Domain;
+using System.Linq;
 
 namespace SamuraiApp.Data
 {
@@ -11,12 +13,10 @@ namespace SamuraiApp.Data
         {
             _ctx = ctx;
         }
-
         public BusinessDataLogic()
         {
             _ctx = new SamuraiContext();
         }
-
         public int AddSamuraisByName(params string[] names)
         {
             foreach (var name in names)
@@ -24,8 +24,21 @@ namespace SamuraiApp.Data
                 _ctx.Samurais.Add(new Samurai { Name = name });
             }
             // Return save changes count - use in testing
-            var dbResult = _ctx.SaveChanges();
-            return dbResult;
+            return _ctx.SaveChanges();
+            
+        }
+        public int InsertNewSamurai(Samurai samurai)
+        {
+            _ctx.Samurais.Add(samurai);
+            return _ctx.SaveChanges();
+        }
+
+        public Samurai GetSamuraiWithQuotes(int samuraiId)
+        {
+            var samuraiWithQuotes = _ctx.Samurais.Where(s => s.Id == samuraiId)
+                                                 .Include(s => s.Quotes)
+                                                 .FirstOrDefault();
+            return samuraiWithQuotes;
         }
     }
 }
--- SamuraiApp.Test/InMemoryDatabaseTests.cs
+++ SamuraiApp.Test/InMemoryDatabaseTests.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SamuraiApp.Data;
 using SamuraiApp.Data.Database;
 using SamuraiApp.Domain;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SamuraiApp.Test
 {
@@ -37,6 +39,78 @@ namespace SamuraiApp.Test
             var result = businessLogic.AddSamuraisByName(names);
 
             Assert.AreEqual(names.Length, result);
+        }
+
+        [TestMethod]
+        public void CanInsertSingleMethod()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+            optionsBuilder.UseInMemoryDatabase("CanInsertSingleMethod");
+            using (var ctx = new SamuraiContext(optionsBuilder.Options))
+            {
+                var businessLogic = new BusinessDataLogic(ctx);
+                var samurai = new Samurai { Name = "Tommy" };
+                var result = businessLogic.InsertNewSamurai(samurai);
+            }
+            // Use different context to same Db instance
+            using (var ctx2 = new SamuraiContext(optionsBuilder.Options))
+            {
+                Assert.AreEqual(1, ctx2.Samurais.Count());
+            }
+        }
+
+        [TestMethod]
+        public void CanInsertSamuraiwithQuotes()
+        {
+
+            var samuraiGraph = new Samurai
+            {
+                Name = "Kyuzo",
+                Quotes = new List<Quote> {
+          new Quote { Text = "Watch out for my sharp sword!" },
+          new Quote { Text = "I told you to watch out for the sharp sword! Oh well!" }
+        }
+            };
+            var builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase("SamuraiWithQuotes");
+            using (var context = new SamuraiContext(builder.Options))
+            {
+                var bizlogic = new BusinessDataLogic(context);
+                var result = bizlogic.InsertNewSamurai(samuraiGraph);
+            };
+            using (var context2 = new SamuraiContext(builder.Options))
+            {
+                var samuraiWithQuotes = context2.Samurais.Include(s => s.Quotes).FirstOrDefaultAsync().Result;
+                Assert.AreEqual(2, samuraiWithQuotes.Quotes.Count);
+            }
+        }
+
+        [TestMethod, TestCategory("SamuraiWithQuotes")]
+        public void CanGetSamuraiwithQuotes()
+        {
+            int samuraiId;
+            var builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase("SamuraiWithQuotes");
+            using (var seedcontext = new SamuraiContext(builder.Options))
+            {
+                var samuraiGraph = new Samurai
+                {
+                    Name = "Kyuzo",
+                    Quotes = new List<Quote> {
+                        new Quote { Text = "Watch out for my sharp sword!" },
+                        new Quote { Text = "I told you to watch out for the sharp sword! Oh well!" }
+                    }
+                };
+                seedcontext.Samurais.Add(samuraiGraph);
+                seedcontext.SaveChanges();
+                samuraiId = samuraiGraph.Id;
+            }
+            using (var context = new SamuraiContext(builder.Options))
+            {
+                var bizlogic = new BusinessDataLogic(context);
+                var result = bizlogic.GetSamuraiWithQuotes(samuraiId);
+                Assert.AreEqual(2, result.Quotes.Count);
+            };
         }
     }
 }
